bab507763b8ec09b5727ade09183f113dcc6496e
Joachim <47549862+Schokokex@users.noreply.github.com>
2021-10-14 10:45:07 +0200

    Update postdebootstrap.sh

--- postdebootstrap.sh
+++ postdebootstrap.sh
@@ -3,7 +3,7 @@ echo deb http://deb.debian.org/debian bullseye main contrib non-free > /etc/apt/
 echo debian > /etc/hostname
 
 apt update
-apt install --no-install-recommends linux-image-amd64 systemd-sys nano command-not-found bash-completion network-manager gnome-session gnome-tweaks gnome-terminal gnome-themes-extra 
+apt install --no-install-recommends linux-image-amd64 systemd-sysv nano command-not-found bash-completion network-manager gnome-session gnome-tweaks gnome-terminal gnome-themes-extra 
 
 echo deb http://deb.debian.org/debian bullseye main > /etc/apt/sources.list
 echo deb-src http://deb.debian.org/debian bullseye main >> /etc/apt/sources.list
